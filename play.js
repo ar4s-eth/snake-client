3884f20a9138dd795bfe50e4b6d53ee1078b50a9
ar4sdoteth <ashley.barr@ar4s.com>
2020-12-02 14:20:16 -0700

    separated out the connect function into the client module. code still works

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 //IP: 192.168.1.67 || "localhost" PORT: 50541
 const net = require('net');
-
-//establish a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  //intepret incoming data as text
-  conn.setEncoding('utf8');
-
-  //add event handler 
-  conn.on('data', (data) => {
-    console.log(`Server says: `, data);
-  })
-  
-  return conn;
-}
-
-console.log(`Connecting ... `);
+//call connect module from client.js
+const { connect } = require('./client');
+console.log(`Connecting ...`);
 connect();
