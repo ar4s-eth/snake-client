3ef711ffbd05ad72a5aaaf91dd43c599ff042fa6
ar4sdoteth <ashley.barr@ar4s.com>
2020-12-02 17:11:23 -0700

    added the connect callback to setupInput, and updated input.js to accept conn as an argument which will in turn map the connection variable to conn so that handleUserInput can access it

--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+let connection;
+
 //setup user input via stdin
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncoding('uft8');
+  stdin.setEncoding('utf8');
   stdin.resume();
   //when the client receives 'data' from the server, we reference the callback handleUserInput
   //to log valid keystrokes
@@ -13,6 +16,14 @@ const setupInput = () => {
 const handleUserInput = ('data', (keyInput) => {
     if (keyInput === '\u0003') {
       process.exit();
+    } else if (keyInput === 'w') {
+      connection.write('Move: up')
+    } else if (keyInput === 's') {
+      connection.write('Move: down')
+    } else if (keyInput === 'a') {
+      connection.write('Move: left')
+    } else if (keyInput === 'd') {
+      connection.write('Move: right')
     }
 });
 
--- play.js
+++ play.js
@@ -1,5 +1,4 @@
-//IP: 192.168.1.67 || "localhost" PORT: 50541
-const net = require('net');
+
 //call connect module from client.js
 const { connect } = require('./client.js');
 //call setupInput module (which calls to handleUserInput)
@@ -7,23 +6,7 @@ const { setupInput } = require('./input.js')
 
 console.log(`Connecting ...`);
 
-connect();
-
-setupInput();
+// connect() === conn
 
 //setup for user input via stdin
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('uft8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
-const handleUserInput = ('data', (keyInput) => {
-    if (keyInput === '\u0003') {
-      process.exit();
-    }
-});
+setupInput(connect());
